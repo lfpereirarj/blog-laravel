d982f6abf3e1a4a17d26be4da9f3b50341530083
Luiz Felipe <lf.pereirarj@gmail.com>
2018-06-02 20:26:20 -0300

    Criando o componente Topo

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -17,6 +17,8 @@ window.Vue = require('vue');
 
 Vue.component('example-component', require('./components/ExampleComponent.vue'));
 
+Vue.component('header-component', require('./components/Header.vue'));
+
 const app = new Vue({
     el: '#app'
 });
